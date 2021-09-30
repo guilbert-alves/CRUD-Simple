3c9cbb0c05796d046df7869422b385275e0056bc
Harr61 <guilbert.alves6162@gmail.com>
2021-09-30 08:52:09 -0500

    editar

--- src/main/java/br/gov/ac/sefaz/crud/rh/Controler/PessoaControler.java
+++ src/main/java/br/gov/ac/sefaz/crud/rh/Controler/PessoaControler.java
@@ -6,8 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class PessoaControler {
@@ -36,6 +39,18 @@ public class PessoaControler {
     public String salvarPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
         pessoaRepo.save(pessoa);
         return "redirect:/rh/pessoas";
+
+    }
+
+    @GetMapping(path = "/rh/pessoas/{id}")
+    public String alterarPessoa(@PathVariable("id") long id, Model model){
+        Optional<Pessoa> pessoaOpt = pessoaRepo.findById(id);
+        if (pessoaOpt.isEmpty()){
+            throw new IllegalArgumentException("não encontrou");
+        }
+
+        model.addAttribute("pessoa", pessoaOpt.get());
+        return "rh/pessoas/form";
 
     }
 
